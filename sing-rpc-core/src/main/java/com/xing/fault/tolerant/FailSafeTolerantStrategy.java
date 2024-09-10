ae7e832e560f64ca6ea632cd1a18c29e56a5cdde
WangXing <19846811030@163.com>
2024-09-10 19:22:00 +0800

    fix: failSafe容错机制

--- sing-rpc-core/src/main/java/com/xing/fault/tolerant/FailSafeTolerantStrategy.java
+++ sing-rpc-core/src/main/java/com/xing/fault/tolerant/FailSafeTolerantStrategy.java
@@ -7,12 +7,14 @@ import java.util.Map;
 
 /**
  * 静默容错机制
+ * 如果出现了异常，将异常捕获，记录日志，返回空对象。
  */
 @Slf4j
 public class FailSafeTolerantStrategy implements TolerantStrategy{
     @Override
     public RpcResponse doTolerant(Map<String, Object> context, Exception e) {
-        log.info("静默异常处理:{}",e.getMessage());
+
+        log.error("静默异常处理:{}",e.getMessage());
         //直接返回一个空对象
         return new RpcResponse();
     }
